3bda970c38d3e32c3a0444c4c493d644e4825609
skarki1200 <karkisunil1200@gmail.com>
2022-06-21 06:54:05 -0700

    Added styles on button

--- screen/ManageExpense.js
+++ screen/ManageExpense.js
@@ -1,5 +1,8 @@
 import { StyleSheet, Text, View } from "react-native";
 import React, { useLayoutEffect } from "react";
+import IconButton from "../components/UI/IconButton";
+import { GlobalStyles } from "../constants/styles";
+import Button from "../components/UI/Button";
 
 const ManageExpense = ({ route, navigation }) => {
   // iif there is a id then it loads differnetly than when it has id
@@ -13,13 +16,54 @@ const ManageExpense = ({ route, navigation }) => {
     });
   }, [navigation, isEditing]);
 
+  function deleteExpeneseHandler() {}
+
+  function cancelHandler() {}
+
   return (
-    <View>
-      <Text>ManageExpenses</Text>
+    <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button}>{isEditing ? "Update" : "Add"}</Button>
+      </View>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={35}
+            onPress={deleteExpeneseHandler}
+          />
+        </View>
+      )}
     </View>
   );
 };
 
 export default ManageExpense;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+
+  button: {
+    minWidth: 150,
+    marginHorizontal: 28,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+});
