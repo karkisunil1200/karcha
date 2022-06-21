d104c3ec1e76f6f7d32c8bfb5cd1762bcf712dea
skarki1200 <karkisunil1200@gmail.com>
2022-06-20 19:32:26 -0700

    Added Manage screen dynamically

--- screen/ManageExpense.js
+++ screen/ManageExpense.js
@@ -1,7 +1,18 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useLayoutEffect } from "react";
 
-const ManageExpense = () => {
+const ManageExpense = ({ route, navigation }) => {
+  // iif there is a id then it loads differnetly than when it has id
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
+
+  // its like useEffect but better and smoother
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
   return (
     <View>
       <Text>ManageExpenses</Text>
