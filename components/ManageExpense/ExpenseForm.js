c2e85278b6efc4ca9736b021e952f073e09159f9
skarki1200 <karkisunil1200@gmail.com>
2022-06-22 08:46:38 -0700

    Added input form

---
+++ components/ManageExpense/ExpenseForm.js
@@ -0,0 +1,39 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Input from "./Input";
+
+const ExpenseForm = () => {
+  function amountChangeHandler() {}
+
+  return (
+    <View>
+      <Input
+        label="Amount"
+        textInputConfig={{
+          keyboardType: "decimal-pad",
+          onChangeText: amountChangeHandler,
+        }}
+      />
+      <Input
+        label="Date"
+        textInputConfig={{
+          placeholder: "YYY-MM-DD",
+          maxLength: 10,
+          onChangeText: () => {},
+        }}
+      />
+      <Input
+        label="Description"
+        textInputConfig={{
+          multiline: true,
+          onChangeText: () => {},
+          autocorrect: false,
+        }}
+      />
+    </View>
+  );
+};
+
+export default ExpenseForm;
+
+const styles = StyleSheet.create({});
--- screen/ManageExpense.js
+++ screen/ManageExpense.js
@@ -4,6 +4,7 @@ import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
 import Button from "../components/UI/Button";
 import { ExpensesContext } from "../store/expenses-context";
+import ExpenseForm from "../components/ManageExpense/ExpenseForm";
 
 const ManageExpense = ({ route, navigation }) => {
   // iif there is a id then it loads differnetly than when it has id
@@ -46,6 +47,7 @@ const ManageExpense = ({ route, navigation }) => {
 
   return (
     <View style={styles.container}>
+      <ExpenseForm />
       <View style={styles.buttons}>
         <Button style={styles.button} mode="flat" onPress={cancelHandler}>
           Cancel
